5d1c5b078356076b781b36d7f69aaafa41c59263
korfu92 <konrad.korf@gmail.com>
2018-11-25 09:29:27 +0100

    Add project files.

---
+++ CQRSDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CQRSDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
